41d30a7af3f05029f93dea6b186439b981127edb
Dariusz Piwko <littlejohn19@users.noreply.github.com>
2022-03-17 16:22:03 +0100

    Update app.js

--- app/app.js
+++ app/app.js
@@ -4,7 +4,6 @@ import cors from 'cors';
 import express from 'express';
 import mongoose from 'mongoose';
 import morgan from 'morgan';
-import routes from './REST/routes';
 
 const app = express();
 app.use(express.static(__dirname + '/public'));
@@ -15,27 +14,6 @@ app.use(bodyParser.json({limit: '2048kb'}));
 
 app.use(cors());
 
-mongoose.connect(config.databaseUrl, {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useFindAndModify: false
-}, (error) => {
-  if (error) {
-    console.error(error);
-  }
-  else {
-    console.info('Connect with database established');
-  }
-});
-
-process.on('SIGINT', () => {
-  mongoose.connection.close(function () {
-    console.error('Mongoose default connection disconnected through app termination');
-    process.exit(0);
-  });
-});
-
-routes(app);
 
 app.get('/*', function (req, res) {
   res.sendFile(__dirname + '/public/index.html');
